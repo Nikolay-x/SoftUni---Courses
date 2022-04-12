393edd0cff9e9122af2fe3fc75c512d2cd7bb4ec
Nikolay-x <nikolay_x@abv.bg>
2022-04-12 09:15:50 +0300

    Uploading exercise tasks 12-Apr-2022.

--- Programming Basics with Python Nov 2021/03. First Steps in Coding - Lab/01. hello_softuni.py
+++ Programming Basics with Python Nov 2021/03. First Steps in Coding - Lab/01. hello_softuni.py
@@ -1,3 +1,4 @@
-# команда за печатане на текста "Hello SoftUni"
+# 1.Конзолна програма "Hello SoftUni"
+# Напишете конзолна Python програма, която отпечатва текста "Hello SoftUni".
 
 print('Hello SoftUni')

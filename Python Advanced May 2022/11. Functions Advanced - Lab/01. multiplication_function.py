f6b5401ac44040e5fca3083948e38fbc334cc13b
Nikolay-x <nikolay_x@abv.bg>
2022-05-31 14:59:09 +0300

    Uploading exercise tasks 31-May-2022.

---
+++ Python Advanced May 2022/11. Functions Advanced - Lab/01. multiplication_function.py
@@ -0,0 +1,23 @@
+# 1.Multiplication Function
+# Write a function called multiply that can receive any quantity of numbers (integers) as different parameters and returns the result of the multiplication of all of them. Submit only your function in the Judge system.
+#
+# Test Code
+# print(multiply(1, 4, 5))
+# print(multiply(4, 5, 6, 1, 3))
+# print(multiply(2, 0, 1000, 5000))
+#
+# Output
+# 20
+# 360
+# 0
+
+def multiply(*args):
+    result = 1
+    for value in args:
+        result *= value
+    return result
+
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))

7c4e6e7801062d46c91b9a5b6d6dab5c7b979016
Nikolay-x <nikolay_x@abv.bg>
2022-06-28 16:58:48 +0300

    Uploading exercise tasks 28-June-2022.

---
+++ Python OOP June 2022/03. First Steps in OOP - Lab/03. class_book.py
@@ -0,0 +1,29 @@
+# 3.Class Book
+# Create a class called Book. It should have an __init__() method that should receive:
+# name: string
+# author: string
+# pages: int
+# Submit only the class in the judge system.
+#
+# Test Code
+# book = Book("My Book", "Me", 200)
+# print(book.name)
+# print(book.author)
+# print(book.pages)
+#
+# Output
+# My Book
+# Me
+# 200
+
+class Book:
+    def __init__(self, name, author, pages):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+
+book = Book("My Book", "Me", 200)
+print(book.name)
+print(book.author)
+print(book.pages)

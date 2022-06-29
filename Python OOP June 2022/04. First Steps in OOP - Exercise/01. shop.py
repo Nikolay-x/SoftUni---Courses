fc90c4d0d532587102d6863df107b8a580ac5b37
Nikolay-x <nikolay_x@abv.bg>
2022-06-29 11:51:43 +0300

    Uploading exercise tasks 29-June-2022.

---
+++ Python OOP June 2022/04. First Steps in OOP - Exercise/01. shop.py
@@ -0,0 +1,21 @@
+# 1.Shop
+# Create a class called Shop. Upon initialization, it should receive a name (string) and items (list). Create a method called get_items_count() which should return the number of items in the store.
+#
+# Test Code
+# shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+# print(shop.get_items_count())
+#
+# Output
+# 3
+
+class Shop:
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
+
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())

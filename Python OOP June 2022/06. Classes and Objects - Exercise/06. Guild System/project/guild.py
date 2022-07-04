01a565b77c752f8d04dbc20dea661d860dd69b4f
Nikolay-x <nikolay_x@abv.bg>
2022-07-04 20:39:43 +0300

    Uploading exercise tasks 04-July-2022.

---
+++ Python OOP June 2022/06. Classes and Objects - Exercise/06. Guild System/project/guild.py
@@ -0,0 +1,30 @@
+from project.player import Player
+
+
+class Guild:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player):
+        if self.name == player.guild:
+            return f"Player {player.name} is already in the guild."
+        if player.guild != Player.DEFAULT_GUILD:
+            return f"Player {player.name} is in another guild."
+        self.players.append(player)
+        player.guild = self.name
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name):
+        for p in self.players:
+            if p.name == player_name:
+                self.players.remove(p)
+                p.guild = Player.DEFAULT_GUILD
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        result = f"Guild: {self.name}\n"
+        for p in self.players:
+            result += p.player_info() + "\n"
+        return result.strip()

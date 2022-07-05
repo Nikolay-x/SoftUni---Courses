56b1a6f817e711dc28d4769c3d284af6a8cb0c94
Nikolay-x <nikolay_x@abv.bg>
2022-07-05 11:53:50 +0300

    Uploading exercise tasks 05-July-2022.

---
+++ Python OOP June 2022/07. Inheritance - Lab/02. Single Inheritance/main.py
@@ -0,0 +1,17 @@
+# 2.Single Inheritance
+# In a folder called project create two files: animal.py and dog.py:
+# In the animal.py file, create a class called Animal with a single method eat() that returns: "eating…".
+# In the dog.py file, create a class called Dog with a single method bark() that returns: "barking…".
+# The Dog should inherit from Animal.
+# Submit in Judge a zip file of the folder project.
+
+from project.animal import Animal
+from project.dog import Dog
+
+animal = Animal()
+dog = Dog()
+
+print(animal.eat())
+print()
+print(dog.eat())
+print(dog.bark())

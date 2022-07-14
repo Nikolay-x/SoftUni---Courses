a029f5378a76101d7b88c21c4eb5813a49ba4405
Nikolay-x <nikolay_x@abv.bg>
2022-07-14 14:52:40 +0300

    Uploading exercise tasks 14-July-2022.

---
+++ Python OOP June 2022/12. Static and Class Methods - Exercise/04. Gym/project/trainer.py
@@ -0,0 +1,15 @@
+class Trainer:
+    ID = 1
+
+    def __init__(self, name):
+        self.name = name
+        self.id = self.get_next_id()
+
+    @staticmethod
+    def get_next_id():
+        id = Trainer.ID
+        Trainer.ID += 1
+        return id
+
+    def __repr__(self):
+        return f"Trainer <{self.id}> {self.name}"

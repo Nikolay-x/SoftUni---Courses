c76755d4ecc0287c7756fe52d219c28602446088
Nikolay-x <nikolay_x@abv.bg>
2022-07-15 11:04:54 +0300

    Uploading exercise tasks 15-July-2022.

---
+++ Python OOP June 2022/13. Polymorphism and Abstraction - Lab/02. image_area.py
@@ -0,0 +1,62 @@
+# 2.ImageArea
+# Create a class called ImageArea which will store the width and the height of an image. Create a method called get_area() which will return the area of the image. We have also to implement all the magic methods for comparison of two image areas (>, >=, <, <=, ==, !=), which will compare their areas.
+#
+# Test Code
+# a1 = ImageArea(7, 10)
+# a2 = ImageArea(35, 2)
+# a3 = ImageArea(8, 9)
+# print(a1 == a2)
+# print(a1 != a3)
+#
+# Output
+# True
+# True
+
+class ImageArea:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __gt__(self, other):
+        return self.get_area() > other.get_area()
+
+    def __ge__(self, other):
+        return self.get_area() >= other.get_area()
+
+    def __lt__(self, other):
+        return self.get_area() < other.get_area()
+
+    def __le__(self, other):
+        return self.get_area() <= other.get_area()
+
+    def __eq__(self, other):
+        """
+        Overwriting the equal method (__eq__(self, other)) is not a good practice,
+        this is only for the exercise purpose.
+        """
+        return self.get_area() == other.get_area()
+
+    def __ne__(self, other):
+        return self.get_area() != other.get_area()
+
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 == a2)
+print(a1 != a3)
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 != a2)
+print(a1 >= a3)
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 <= a2)
+print(a1 < a3)

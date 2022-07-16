9dfb69bf10eb2c90ec3e1cda9528c9ef8ba8370a
Nikolay-x <nikolay_x@abv.bg>
2022-07-16 14:05:43 +0300

    Uploading exercise tasks 16-July-2022.

---
+++ Python OOP June 2022/14. Polymorphism and Abstraction - Exercise/04. Wild Farm/project/animals/birds.py
@@ -0,0 +1,33 @@
+from project.animals.animal import Bird
+
+
+class Owl(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def preferred_food(self):
+        return ["Meat"]
+
+    @property
+    def weight_increase(self):
+        return 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def preferred_food(self):
+        return ["Vegetable", "Fruit", "Meat", "Seed"]
+
+    @property
+    def weight_increase(self):
+        return 0.35
+
+    def make_sound(self):
+        return "Cluck"

206f1cc8ad1c94599e7acde892477ada72f20ea4
Nikolay-x <nikolay_x@abv.bg>
2022-07-27 12:18:54 +0300

    Uploading exercise tasks 27-July-2022.

---
+++ Python OOP June 2022/20. Decorators - Exercise/02. even_parameters.py
@@ -0,0 +1,43 @@
+# 2.Even Parameters
+# Create a decorator function called even_parameters. It should check if all parameters passed to a function are even numbers and only then execute the function and return the result. Otherwise, don't execute the function and return "Please use only even numbers!"
+#
+# Test Code
+# @even_parameters
+# def add(a, b):
+#     return a + b
+#
+# print(add(2, 4))
+# print(add("Peter", 1))
+#
+# Output
+# 6
+# Please use only even numbers!
+
+def even_parameters(func_ref):
+    def wrapper(*args):
+        for arg in args:
+            if not isinstance(arg, int) or arg % 2 != 0:
+                return "Please use only even numbers!"
+        result = func_ref(*args)
+        return result
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))

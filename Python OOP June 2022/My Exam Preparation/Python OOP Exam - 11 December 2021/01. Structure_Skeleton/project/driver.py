6c8a67b56435726c08d1238db8f874a16392ff15
Nikolay-x <nikolay_x@abv.bg>
2022-08-04 15:49:15 +0300

    Uploading exercise tasks 04-August-2022.

---
+++ Python OOP June 2022/My Exam Preparation/Python OOP Exam - 11 December 2021/01. Structure_Skeleton/project/driver.py
@@ -0,0 +1,21 @@
+from project.car.car import Car
+
+
+class Driver:
+    def __init__(self, name: str):
+        self.name = name
+        self.car = None
+        self.number_of_wins = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value is None or value == "" or value.isspace():
+            raise ValueError("Name should contain at least one character!")
+        self.__name = value
+
+    def add_win(self):
+        self.number_of_wins += 1

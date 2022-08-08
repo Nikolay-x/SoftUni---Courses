8e09d18168848e787e98d1a37f18104cb6d694bf
Nikolay-x <nikolay_x@abv.bg>
2022-08-08 15:24:17 +0300

    Uploading exercise tasks 08-August-2022.

---
+++ Python OOP June 2022/My Exam Preparation/Python OOP Exam - 15 August 2021/01. Structure_Skeleton/project/table/inside_table.py
@@ -0,0 +1,11 @@
+from project.table.table import Table
+
+
+class InsideTable(Table):
+    _MIN_NUMBER = 1
+    _MAX_NUMBER = 50
+    _TABLE_CLASS = 'Inside'
+
+    def __init__(self, table_number: int, capacity: int):
+        self.table_number = table_number
+        super().__init__(self.table_number, capacity)

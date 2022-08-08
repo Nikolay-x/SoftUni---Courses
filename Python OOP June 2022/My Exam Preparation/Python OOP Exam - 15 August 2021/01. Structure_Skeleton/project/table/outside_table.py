d77ba5b35d181205660a45524803d683f85f738b
Nikolay-x <nikolay_x@abv.bg>
2022-08-08 15:24:35 +0300

    Uploading exercise tasks 08-August-2022.

---
+++ Python OOP June 2022/My Exam Preparation/Python OOP Exam - 15 August 2021/01. Structure_Skeleton/project/table/outside_table.py
@@ -0,0 +1,11 @@
+from project.table.table import Table
+
+
+class OutsideTable(Table):
+    _MIN_NUMBER = 51
+    _MAX_NUMBER = 100
+    _TABLE_CLASS = 'Outside'
+
+    def __init__(self, table_number: int, capacity: int):
+        self.table_number = table_number
+        super().__init__(self.table_number, capacity)

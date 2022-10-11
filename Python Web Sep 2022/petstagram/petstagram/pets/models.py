972a57087bb785247a8b2eb6c724889c903069cb
Nikolay-x <nikolay_x@abv.bg>
2022-10-11 20:02:34 +0300

    Uploading exercise tasks 11-October-2022.

--- Python Web Sep 2022/petstagram/petstagram/pets/models.py
+++ Python Web Sep 2022/petstagram/petstagram/pets/models.py
@@ -1,3 +1,55 @@
+# pets/models.py
+
 from django.db import models
+from django.utils.text import slugify
+
+from petstagram.core.model_mixins import StrFromFieldsMixin
+
 
 # Create your models here.
+
+
+class Pet(StrFromFieldsMixin, models.Model):
+    str_fields = ('id', 'name',)
+
+    MAX_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_NAME,
+        null=False,
+        blank=False,
+    )
+
+    personal_photo = models.URLField(
+        null=False,
+        blank=False,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        # Create/Update
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f'{self.id}-{self.name}')
+
+        # Without 'if not' the following scenario might happen:
+        # The url is '/pets/4-stamat'
+        # Rename 'stamat' to 'stamata'
+        # The new url is '/pets/4-stamata', but the old url '/pets/4-stamat' does not work
+
+        # Update
+        return super().save(*args, **kwargs)
+
+    # def __str__(self):
+    #     return f'ID={self.id}, Name={self.name}'
--- Python Web Sep 2022/petstagram/petstagram/photos/models.py
+++ Python Web Sep 2022/petstagram/petstagram/photos/models.py
@@ -1,3 +1,65 @@
+# photos/models.py
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+from petstagram.core.model_mixins import StrFromFieldsMixin
+from petstagram.pets.models import Pet
+from petstagram.photos.validators import validate_file_less_than_5mb
+
+
 # Create your models here.
+
+
+class Photo(StrFromFieldsMixin, models.Model):
+    str_fields = ('pk', 'photo', 'location')
+
+    MIN_DESCRIPTION_LENGTH = 10
+    MAX_DESCRIPTION_LENGTH = 300
+
+    MAX_LOCATION_LENGTH = 30
+
+    # Requires media files to work correctly
+    photo = models.ImageField(
+        upload_to='mediafiles/pet_photos/',
+        null=False,
+        blank=True,
+        validators=(
+            validate_file_less_than_5mb,
+        ),
+    )
+
+    description = models.CharField(
+        # DB validation
+        max_length=MAX_DESCRIPTION_LENGTH,
+        validators=(
+            # Django/python validation, not DB validation
+            MinLengthValidator(MIN_DESCRIPTION_LENGTH),
+        ),
+        null=True,
+        blank=True,
+    )
+
+    location = models.CharField(
+        max_length=MAX_LOCATION_LENGTH,
+        null=True,
+        blank=True,
+    )
+
+    publication_date = models.DateField(
+        # Automatically sets current date on 'save' (create or update)
+        auto_now=True,
+        null=False,
+        blank=True,
+    )
+
+    # One-to-one relations
+    #
+    # One-to-many relations
+    #
+    # Many-to-many relations
+
+    tagged_pets = models.ManyToManyField(
+        Pet,
+        blank=True,
+    )

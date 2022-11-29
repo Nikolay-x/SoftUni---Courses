22478c9ea87d168ffdfa7e96d5d40f8746807ebe
Nikolay-x <nikolay_x@abv.bg>
2022-11-29 10:02:29 +0200

    Uploading exercise tasks 29-November-2022.

---
+++ Python Web Framework Oct 2022/err_exc_handling/custom_users/serializers.py
@@ -0,0 +1,28 @@
+import bcrypt
+from rest_framework import serializers
+
+from custom_users.models import CustomUser
+from custom_users.validators import validate_password
+from err_exc_handling.settings import SALTING
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        exclude = ('password',)
+
+
+class CustomUserInSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = CustomUser
+        validators = [validate_password]
+
+    def create(self, validated_data):
+        validated_data["password"] = bcrypt.hashpw(validated_data["password"].encode(), SALTING.encode())
+        user = CustomUser(**validated_data)
+        try:
+            user.save()
+        except Exception:
+            a = 5
+        return user

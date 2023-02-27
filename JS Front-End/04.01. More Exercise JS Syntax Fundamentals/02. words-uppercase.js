9d82c67815aa1a99054606d204bcea45964cca7c
Nikolay-x <nikolay_x@abv.bg>
2023-02-27 10:59:48 +0200

    Uploading exercise tasks 27-Feb-2023.

--- JS Front-End/04.01. More Exercise JS Syntax Fundamentals/02. words-uppercase.js
+++ JS Front-End/04.01. More Exercise JS Syntax Fundamentals/02. words-uppercase.js
@@ -1,32 +1,32 @@
-// function wordsUppercase(text) {
-//   function isLetter(character) {
-//     return (
-//       character &&
-//       character.length === 1 &&
-//       character.toLowerCase() !== character.toUpperCase()
-//     );
-//   }
+function wordsUppercase(text) {
+  function isLetter(character) {
+    return (
+      character &&
+      character.length === 1 &&
+      character.toLowerCase() !== character.toUpperCase()
+    );
+  }
 
-//   let textArr = text.split(" ");
-//   let result = [];
-//   for (const word of textArr) {
-//     let currentWord = "";
-//     for (const character of word) {
-//       if (isLetter(character)) {
-//         currentWord += character.toUpperCase();
-//       } else {
-//         if (currentWord) {
-//           result.push(currentWord);
-//           currentWord = "";
-//         }
-//       }
-//     }
-//     if (currentWord) {
-//       result.push(currentWord);
-//     }
-//   }
-//   console.log(result.join(", "));
-// }
+  let textArr = text.split(" ");
+  let result = [];
+  for (const word of textArr) {
+    let currentWord = "";
+    for (const character of word) {
+      if (isLetter(character) || !isNaN(character)) {
+        currentWord += character.toUpperCase();
+      } else {
+        if (currentWord) {
+          result.push(currentWord);
+          currentWord = "";
+        }
+      }
+    }
+    if (currentWord) {
+      result.push(currentWord);
+    }
+  }
+  console.log(result.join(", "));
+}
 
 function regexWordsUpperCase(text) {
   const words = text.match(/\b\w+\b/g) || [];
@@ -34,10 +34,12 @@ function regexWordsUpperCase(text) {
   console.log(upperCaseWords.join(", "));
 }
 
-// wordsUppercase("Hi, how are you?");
-// wordsUppercase("hellow");
-// wordsUppercase("? kjqbfw idfkewq,fwqf   ");
+wordsUppercase("Hi, how are you?");
+wordsUppercase("hellow");
+wordsUppercase("? kjqbfw idfkewq,fwqf   ");
+wordsUppercase("? 23bfw iw3q,fwqf1   ");
 
 regexWordsUpperCase("Hi, how are you?");
 regexWordsUpperCase("hellow");
 regexWordsUpperCase("? kjqbfw idfkewq,fwqf   ");
+wordsUppercase("? 23bfw iw3q,fwqf1   ");

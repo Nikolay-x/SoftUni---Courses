2e564abbf0d8e5449fb7582f1d58f755c21c59f6
Nikolay-x <nikolay_x@abv.bg>
2023-03-18 17:55:02 +0200

    Uploading exercise tasks 18-Mar-2023.

--- JS Front-End/230316 10. Exercise DOM and Events/01. Subtraction/subtract.js
+++
@@ -1,5 +0,0 @@
-function subtract() {
-  const numOne = Number(document.getElementById("firstNumber").value);
-  const numTwo = parseFloat(document.querySelector("#secondNumber").value);
-  document.querySelector("#result").textContent = numOne - numTwo;
-}

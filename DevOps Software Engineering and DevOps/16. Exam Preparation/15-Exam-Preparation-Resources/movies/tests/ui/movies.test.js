de6e410e1b95f8deb9d708bf774a94c81e42e706
Nikolay-x <nikolay_x@abv.bg>
2023-08-09 13:05:51 +0300

    sync

--- DevOps Software Engineering and DevOps/16. Exam Preparation/15-Exam-Preparation-Resources/movies/tests/ui/movies.test.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Check movies page', async ({ page }) => {
-    await page.goto('http://localhost:8080/collection');
-    const list = await page.$('ul');
-    expect(list).toBeTruthy();
-  });
-  
--- DevOps Software Engineering and DevOps/16. Exam Preparation/Movies solution/ui-tests/movies.test.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Check movies page', async ({ page }) => {
-    // await page.goto('http://localhost:8080/movies');
-    await page.goto('https://nikolay-x-movies.onrender.com/movies');
-    const list = await page.$('ul');
-    expect(list).toBeTruthy();
-  });
-  
